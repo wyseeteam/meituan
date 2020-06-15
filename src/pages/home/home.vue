4ecb7e9e8c10cb80bec9e211005e3bcef9c38ba1
WYwy123456654321 <wyseeteam@hotmail.com>
2020-06-15 14:42:50 +0800

    首页组件

---
+++ src/pages/home/home.vue
@@ -0,0 +1,14 @@
+<template>
+        <div>dhsadjhj</div>
+</template>
+<script>
+      export default {
+        name: '',
+        components: {
+    
+    
+            }
+      }
+</script>
+<style>
+</style>
