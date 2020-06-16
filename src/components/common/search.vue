dd22e72792429b8cd65427dba6376eedacaa1739
WYwy123456654321 <wyseeteam@hotmail.com>
2020-06-16 17:48:00 +0800

    search

--- src/components/common/search.vue
+++ src/components/common/search.vue
@@ -1,26 +1,63 @@
 <template>
 	<div class="searchwrapper">
 		<img src="../../assets/logo.png">
-		<div class="searchbtn">
-			<input type="text" name="" placeholder="搜索商家或地点">
-			<button>搜索</button>
+		<div class="searchmodule">
+			<div class="searchbtn">
+				<input type="text" name="" placeholder="搜索商家或地点" @input="getTip" v-model="keyword">
+				<button>搜索</button>
+			</div>
+			
 		</div>
 	</div>
 </template>
 <script>
-     export default {
-      	name: '',
-        components: {
-    
-    
-            }
-      }
+import {request_get} from '../../ajax/request.js';
+
+export default {
+ 	data(){
+ 		return {
+ 			keyword: '',//搜索关键字
+ 		}
+ 	},
+  	methods:{
+  		getTip(){
+			console.log(this.keyword)
+      		 request_get('/suggest?keyword='+this.keyword);
+
+  		}
+  	}
+ }
 </script>
 <style>
 .searchwrapper{
 	width: 1200px;
 	margin: 0 auto;
-	display: flex;
-	justify-content: space-between;
+	position: relative;
+	text-align: center;
+}
+.searchwrapper img{
+	position: absolute;
+	left: 0;
+}
+.searchmodule{
+	text-align: center;
+}
+.searchbtn{
+	display: inline-block;
+	width: 550px;
+	height: 40px;
+	font-size: 0;
+}
+.searchbtn>*{
+	height: 100%;
+	vertical-align: top;
+}
+.searchbtn input{
+	width: 80%;
+	padding: 10px;
+}
+.searchbtn button{
+	width: 20%;
+	background-color: #FFC300;
 }
 </style>
