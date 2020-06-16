2a9cdc9a0ccd5f75531173dd968fff459651fe00
WYwy123456654321 <wyseeteam@hotmail.com>
2020-06-16 09:42:02 +0800

    修改默认样式

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,14 @@ export default {
 </script>
 
 <style>
+body{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 }
 
 h1,
