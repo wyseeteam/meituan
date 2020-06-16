215447a89b8f6386596717cec8181a221dcba122
weiwei <wyseeteam@hotmail.com>
2020-06-16 11:00:59 +0800

    请求数据

--- src/pages/home/content/contentBottom/contentBottom.vue
+++ src/pages/home/content/contentBottom/contentBottom.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>contentBottom</div>
+  <div>
+    <div>猫眼电影</div>
+    <div>推荐民宿</div>
+    <div>猜你喜欢</div>
+    <div>推荐民宿</div>
+  </div>
+  
+
 </template>
 <script>
+
+import {request_get} from '../../../../ajax/request.js';
+  
 export default {
   name: "",
-  components: {}
+  components: {
+    
+  },
+
+  created: function()
+  {
+    console.log('created');
+    this.$options.methods.requestFilms();
+  },
+
+  methods: {
+    requestFilms: function()
+    {
+      console.log('请求数据');
+      request_get('https://hf.meituan.com/ptapi/getComingFilms?ci=56&limit=10', null, null);
+    }
+  }
 };
 </script>
 <style>
